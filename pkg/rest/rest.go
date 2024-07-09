eda672ee278f1b402fec2519aaa2320cbde27a3c
Wim Henderickx <wim.henderickx@gmail.com>
2024-07-09 17:54:00 +0200

    added resetfielstrategy

---
+++ pkg/rest/rest.go
@@ -0,0 +1,9 @@
+package rest
+
+import "sigs.k8s.io/structured-merge-diff/v4/fieldpath"
+
+// ResetFieldsStrategy is an optional interface that a storage object can
+// implement if it wishes to provide the fields reset by its strategies.
+type ResetFieldsStrategy interface {
+	GetResetFields() map[fieldpath.APIVersion]*fieldpath.Set
+}
